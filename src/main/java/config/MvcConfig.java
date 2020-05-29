31d4b395fcc9ba44b13f333fdd49bcfa02b87d16
ohbeomkwon <ohbeomkwon@DESKTOP-FE310J1>
2020-05-29 16:04:51 +0900

    2nd commit

--- src/main/java/config/MvcConfig.java
+++ src/main/java/config/MvcConfig.java
@@ -1,6 +1,9 @@
 package config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -24,5 +27,13 @@ public class MvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/main").setViewName("main");
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+		ms.setBasename("message.label");	// message 패키지에 속한 label 파일을 불러온다는 이야기
+		ms.setDefaultEncoding("UTF-8");
+		return ms;
 	}
 }
--- src/main/webapp/WEB-INF/view/register/step3.jsp
+++ src/main/webapp/WEB-INF/view/register/step3.jsp
@@ -1,13 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>회원가입</title>
+<title><spring:message code="member.register" /></title>
 </head>
 <body>
-	<p><strong>${registerRequest.name}님. </strong>회원 가입을 완료하였습니다.</p>
-	<p><a href="<c:url value='/main'/>">[첫 화면 이동]</a>
+	<p><spring:message code="register.done" arguments="${registerRequest.name}" /></p>
+	<p><a href="<c:url value='/main'/>">[<spring:message code="go.main" />]</a>
 </body>
 </html>
